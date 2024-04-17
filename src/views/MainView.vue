1c56f20338901015f4f7703a2732f698a665d362
chinyuting <s86052915@gmail.com>
2024-04-17 13:08:00 +0800

    edit stringify

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -87,13 +87,13 @@ const callApi = function () {
     code = location.search.slice(6)
     if (code) {
       axios
-        .post('https://cors-anywhere.herokuapp.com/https://api.instagram.com/oauth/access_token', {
+        .post('https://cors-anywhere.herokuapp.com/https://api.instagram.com/oauth/access_token', JSON.stringify({
           client_id: '461541476203224',
           client_secret: '${client_secret.value}',
           grant_type: 'authorization_code',
           redirect_uri: 'https://chinyuting.github.io/Instagram-Imitation/',
           code: `${code}`
-        })
+        }))
         .then((res) => {
           console.log('response', res)
         })
