baecf22e0ae25f259f9471bbc264a66649b02ea6
chinyuting <s86052915@gmail.com>
2024-04-27 16:03:56 +0800

    add profile data

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -4,32 +4,60 @@ import navComponent from '../components/navComponent.vue'
 import { usePostDataStore } from '../stores/postDataListStore.js'
 import { useUserDataStore } from '../stores/userDataStore.js'
 
-// 引入 postDataStore 呼叫getData方法取得PostDataList並儲存
+/**
+ * 引入 usePostDataStore 呼叫getPostData方法取得PostDataList並儲存
+ */
 const postData = usePostDataStore()
 postData.getPostData()
 
+/**
+ * 引入 useUserDataStore 呼叫getUserData方法取得userData並儲存
+ */
 const userData = useUserDataStore()
 userData.getUserData()
-// console.log(userData.userData.value);
 
 const getPost = function (id) {
   console.log(id)
 }
-
-// onMounted(() => {
-//   console.log('userData.value', userData.value)
-// })
 </script>
 
 <template>
   <div class="row mx-0">
     <navComponent />
     <main class="col m-0 border-start min-vh-100">
-      <div class="profile mx-auto py-md-4">
+      <div class="profile mx-auto py-md-4 d-flex">
         <div class="rounded-circle profile-pic m-1 flex-shrink-0 p-1">
-          <!-- <img src="../assets/images/test.jpg" alt="" /> -->
+          <img src="../assets/images/profile.jpg" alt="" />
         </div>
-        <div>{{ userData.userData.username }}</div>
+        <div class="align-self-center ms-5">
+          <div class="d-flex my-1 align-self-center">
+            <h4 class="userName p-2">{{ userData.userData.username }}</h4>
+            <div>
+              <button class="btn btn-light" type="button">編輯個人檔案</button>
+            </div>
+            <div class="p-2 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-gear-wide"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z"
+                />
+              </svg>
+            </div>
+          </div>
+          <ul class="my-1 d-flex list-unstyled align-self-center">
+            <li class="media_count profileData p-2">
+              {{ userData.userData.media_count }}<span>則貼文</span>
+            </li>
+            <li class="media_count profileData p-2">0<span>位粉絲</span></li>
+            <li class="media_count profileData p-2">0<span>人追蹤中</span></li>
+          </ul>
+        </div>
       </div>
 
       <article class="row mx-auto border-top">
@@ -100,6 +128,14 @@ article {
   &:hover::before {
     opacity: 0.5; //hover改變透明度
   }
+}
+
+.profileData {
+  font-size: 17px;
+}
+
+.cursor-pointer {
+  cursor: pointer;
 }
 
 @media (min-width: 768px) {
