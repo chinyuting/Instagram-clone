9f13b32aa349fb84342c3d601fa488fa37f7c8be
chinyuting <s86052915@gmail.com>
2024-05-31 15:33:12 +0800

    edit getPostOwnerPic

--- src/components/postComponent.vue
+++ src/components/postComponent.vue
@@ -79,9 +79,6 @@ const ThumbsUp = function (post) {
 }
 // 取得post owner pic
 const getPostOwnerPic = (post) => {
-  console.log(userData.value)
-  console.log(post.postownerId)
-  console.log(post.postownerId === userData.value.id)
   if (userData.value && post.postownerId === userData.forEach((user) => user.id)) {
     return userData.value.media_url
   }
