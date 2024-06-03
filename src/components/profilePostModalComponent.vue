154317179afe7746f0d7b0eda03b82c4ffc7e2b9
chinyuting <s86052915@gmail.com>
2024-06-03 20:38:26 +0800

    edit profile post modal

--- src/components/profilePostModalComponent.vue
+++ src/components/profilePostModalComponent.vue
@@ -38,17 +38,16 @@ const props = defineProps({
     aria-hidden="true"
     ref="profilePostModal"
   >
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
+        <!-- <div class="modal-header"> -->
+        <button
+          type="button"
+          class="btn-close btn-close-white position-absolute top-0 end-0 m-3"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+        <!-- </div> -->
         <div class="modal-body">
           <postComponent :postDataList="mergedPostData" />
         </div>
