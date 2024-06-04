54980484b49b89e1d855d2d8f5db4399d1257ef6
chinyuting <s86052915@gmail.com>
2024-06-04 21:31:53 +0800

    add heart

--- src/components/postComponent.vue
+++ src/components/postComponent.vue
@@ -148,6 +148,11 @@ const postMessage = (id) => {
       class="carousel slide post-pic-area position-relative"
       @dblclick="ThumbsUp(post)"
     >
+    <span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill text-danger" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+      </svg>
+    </span>
       <div class="carousel-indicators" v-if="post.media_type === 'CAROUSEL_ALBUM'">
         <div v-for="(img, key) in post.media_url.length" :key="key">
           <button
