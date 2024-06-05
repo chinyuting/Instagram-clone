3ec254d8c5db7f5e68d5ef90244664ba660a46f0
chinyuting <s86052915@gmail.com>
2024-06-05 16:14:20 +0800

    edit post data list store

--- src/stores/postDataListStore.js
+++ src/stores/postDataListStore.js
@@ -4,44 +4,79 @@ import axios from 'axios'
 
 export const usePostDataStore = defineStore('postDataList', () => {
   const postData = ref([])
-
-
-  function getPostData() {
-    const isLoading = ref(true)
-    const access_token = localStorage.getItem('long-lived-access-token');
-    axios
-    .get(
-      `https://cors-anywhere.herokuapp.com/https://graph.instagram.com/me/media?fields=caption,id,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=${access_token}`
-    )
-    .then((res) => {
-      console.log(res)
+  const isLoading = ref(true)
+  const error = ref(null)
+  // function getPostData() {
+  //   const access_token = localStorage.getItem('long-lived-access-token');
+  //   axios
+  //   .get(
+  //     `https://cors-anywhere.herokuapp.com/https://graph.instagram.com/me/media?fields=caption,id,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=${access_token}`
+  //   )
+  //   .then((res) => {
+  //     console.log(res)
+  //     postData.value = res.data.data
+  //     console.log('post', postData.value)
+  //     postData.value.forEach((post) => {
+  //       console.log(post.media_type)
+  //       if(post.media_type === 'CAROUSEL_ALBUM') {
+  //         getMoreImg(post.id)
+  //         // post.media_children = getMoreImg(post.id)
+  //       }
+  //     })
+  //     isLoading.value = false
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //   })
+  // }
+  async function getPostData() {
+    const access_token = localStorage.getItem('long-lived-access-token')
+    try {
+      const res = await axios.get(
+        `https://cors-anywhere.herokuapp.com/https://graph.instagram.com/me/media?fields=caption,id,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=${access_token}`
+      )
       postData.value = res.data.data
       console.log('post', postData.value)
-      postData.value.forEach((post) => {
-        console.log(post.media_type)
-        if(post.media_type === 'CAROUSEL_ALBUM') {
-          getMoreImg(post.id)
-          // post.media_children = getMoreImg(post.id)
+      await Promise.all(postData.value.map(async (post) => {
+        if (post.media_type === 'CAROUSEL_ALBUM') {
+          const mediaChildren = await getMoreImg(post.id)
+          console.log(mediaChildren);
+          post.media_children = mediaChildren
         }
-      })
+      }))
+    } catch (err) {
+      console.error(err)
+      error.value = err
+    } finally {
       isLoading.value = false
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    }
   }
-  function getMoreImg(id) {
-    const access_token = localStorage.getItem('long-lived-access-token');
-    axios
-    .get(
-      `https://graph.instagram.com/${id}/children?access_token=${access_token}`
-    )
-    .then((res) => {
-      console.log(res.data)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  // function getMoreImg(id) {
+  //   const access_token = localStorage.getItem('long-lived-access-token');
+  //   axios
+  //   .get(
+  //     `https://graph.instagram.com/${id}/children?access_token=${access_token}`
+  //   )
+  //   .then((res) => {
+  //     console.log(res.data)
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //   })
+  // }
+  async function getMoreImg(id) {
+    const access_token = localStorage.getItem('long-lived-access-token')
+    try {
+      const res = await axios.get(
+        `https://graph.instagram.com/${id}/children?access_token=${access_token}`
+      )
+      console.log(res.data);
+      return res.data.data
+    } catch (err) {
+      console.error(err)
+      error.value = err
+      return []
+    }
   }
 
   return { postData, getPostData }
