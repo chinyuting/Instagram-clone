95282b651fd07d7a48acf6fcdd0bd56a0c4da43c
chinyuting <s86052915@gmail.com>
2024-06-14 23:23:02 +0800

    edit name/ add comment

--- src/components/addPostModalComponent.vue
+++ src/components/addPostModalComponent.vue
@@ -5,108 +5,151 @@ import { getStorage, uploadBytes, ref as storageRef, getDownloadURL } from 'fire
 import { db, firebaseRef, push, firebaseApp } from '../firebaseSetUp'
 import { useUserDataStore } from '../stores/userDataStore.js'
 
-// isLoading ini
+// 修改註記 todo
+
+// isLoading init
 const isLoading = ref(false)
 
-// modal
+// addPostModal 定義showModal function
 const modal = ref(null)
-const addNewModal = ref(null)
-// 開啟Modal時清空imgSrc欄位
+const addPostModal = ref(null)
 const showModal = function () {
   modal.value.show()
-  imgSrc.value = ''
-  isModalSideShow.value = false
-  modalDialog.value.style.maxWidth = '700px'
-  selectedImg.value = null
+  modalInit()
 }
+
+/**
+ * 確保重置相關欄位值
+ * imgSrc selectedImage 圖片欄位清空
+ * isSideModalShow 側邊modal關閉
+ * modalDialog modal寬度定義
+ */
+const modalInit = () => {
+  imgSrc.value = ''
+  isSideModalShow.value = false
+  modalDialog.value.style.maxWidth = '700px'
+  selectedImage.value = null
+}
+
+// showModal開放父層使用
 defineExpose({
   showModal
 })
 onMounted(() => {
-  modal.value = new Modal(addNewModal.value)
+  modal.value = new Modal(addPostModal.value)
 })
 
 const imgSrc = ref(null)
-const selectedImg = ref(null)
+const selectedImage = ref(null)
 /**
-   ＊ 新增照片並顯示預覽
-   ＊ ＠param {Object} e - input change event
-  */
+ * 選擇照片並顯示預覽
+ * @param {Object} target - input欄位 change event target
+ */
 const addImage = function (target) {
+  // 加入loading
   isLoading.value = true
-  let input = target
-  selectedImg.value = target.files[0]
-
   // 預覽照片
-  if (input.files) {
+  if (target.files) {
+    selectedImage.value = target.files[0]
+    // FileReader讀取file
     let reader = new FileReader()
     reader.onload = (e) => {
       imgSrc.value = e.target.result
+      // 解除loading
       isLoading.value = false
     }
-    // this.image = input.files[0]
-    reader.readAsDataURL(input.files[0])
+    // 檔案轉為url
+    reader.readAsDataURL(target.files[0])
   }
 }
 
 // drag file
 const isDragged = ref(false)
+/**
+ * 檔案拖曳進目標內
+ * @param {Object} event - drag event
+ */
 const handleDragOver = (event) => {
+  // drag鼠標特效
   event.dataTransfer.dropEffect = 'copy'
   isDragged.value = true
   event.preventDefault()
 }
-
+/**
+ * 檔案拖曳離開目標
+ * @param {Object} event - drag event
+ */
 const handleDragLeave = (event) => {
   isDragged.value = false
   event.preventDefault()
-  console.log('DragLeave')
 }
-//  drop
+/**
+ * 檔案drop在目標
+ * @param {Object} event - drop event
+ */
 const handleDrop = (event) => {
+  // drop檔案時避免在網頁開啟檔案
   event.preventDefault()
+  // drop檔案時觸發addImage新增檔案
   addImage(event.dataTransfer)
 }
 
-// 下一步
-// 用isModalSideShow判斷 true->已開啟SideModal 執行uploadImage/ false-> 尚未開啟SideModal 則開啟SideModal
 const modalDialog = ref(null)
-const isModalSideShow = ref(false)
-const showSideModal = function () {
-  if (isModalSideShow.value === true) {
+const isSideModalShow = ref(false)
+/**
+ * 下一步
+ * 用isSideModalShow判斷
+ * true-> 已開啟SideModal 執行uploadImage
+ * false-> 尚未開啟SideModal 則開啟SideModal
+ */
+const toNextStep = function () {
+  if (isSideModalShow.value === true) {
     uploadImage()
   } else {
     modalDialog.value.style.maxWidth = '1000px'
-    isModalSideShow.value = true
+    isSideModalShow.value = true
   }
 }
-// 上一步 回到第一步上傳圖片
+
+/**
+ * 上一步 回到第一步上傳圖片
+ * 用isSideModalShow判斷
+ * true-> 重新定義modalDialog寬度 並關閉side modal
+ */
 const backToPreviousStep = () => {
-  if (isModalSideShow.value === true) {
+  if (isSideModalShow.value === true) {
     modalDialog.value.style.maxWidth = '700px'
-    isModalSideShow.value = false
+    isSideModalShow.value = false
   }
   imgSrc.value = null
 }
 
-// 上傳圖片至firebase storage
+/**
+ * 上傳圖片至firebase storage
+ * 非同步function
+ */
 const uploadImage = async () => {
+  // 加入loading
   isLoading.value = true
-  const file = selectedImg.value
+  // 取得上傳的image檔案
+  const file = selectedImage.value
   if (!file) {
     console.error('No file selected.')
     return
   }
   const storage = getStorage(firebaseApp)
-  const fileRef = storageRef(storage, 'posts/' + file.name)
+  // 設定上傳路徑
+  const fileStorageRef = storageRef(storage, 'posts/' + file.name)
   try {
     //上傳照片至firebase storage
-    await uploadBytes(fileRef, file)
-    //取得上傳後的url
-    const imageUrl = await getDownloadURL(fileRef)
-    console.log(imageUrl)
+    await uploadBytes(fileStorageRef, file)
+    //等待上傳照片後取得上傳後的url
+    const imageUrl = await getDownloadURL(fileStorageRef)
+    // 成功上傳並取得imageUrl後執行 pushPostToFirebase 失敗則重新上傳
     if (imageUrl) {
       pushPostToFirebase(imageUrl)
+    } else {
+      uploadImage()
     }
   } catch (error) {
     isLoading.value = false
@@ -118,20 +161,25 @@ const uploadImage = async () => {
 const userData = useUserDataStore()
 
 const postCaption = ref('')
-// post push firebase
+
+/**
+ * 將post push到firebase
+ * @param {String} imageUrl - 已上傳至firbase storage的image url
+ */
 const pushPostToFirebase = function (imageUrl) {
-  //  post資料
-  const postId = Date.now()
+  // 設定上傳路徑
   const itemsRef = firebaseRef(db, 'postsData')
+  //  建立post資料
+  const currentTime = Date.now()
   const newPostData = {
     caption: postCaption.value,
-    id: postId + userData.userData.id,
+    id: currentTime + userData.userData.id,
     isThumb: false,
     media_type: 'IMAGE',
     media_url: [imageUrl],
     permalink: '',
     postownerid: userData.userData.id,
-    timestamp: new Date().toISOString(),
+    timestamp: currentTime.toISOString(),
     username: userData.userData.username
   }
   push(itemsRef, newPostData)
@@ -139,11 +187,12 @@ const pushPostToFirebase = function (imageUrl) {
       console.log('Data successfully written to Firebase!')
       modal.value.hide()
       postCaption.value = ''
-      isLoading.value = false
     })
     .catch((error) => {
-      isLoading.value = false
       console.error('Error writing data to Firebase:', error)
+    })
+    .finally(() => {
+      isLoading.value = false
     })
 }
 </script>
@@ -152,11 +201,11 @@ const pushPostToFirebase = function (imageUrl) {
   <!-- Modal -->
   <div
     class="modal fade"
-    id="exampleModal"
+    id="addPostModal"
     tabindex="-1"
-    aria-labelledby="exampleModalLabel"
+    aria-labelledby="addPostModalLabel"
     aria-hidden="true"
-    ref="addNewModal"
+    ref="addPostModal"
   >
     <button
       type="button"
@@ -174,12 +223,12 @@ const pushPostToFirebase = function (imageUrl) {
           >
             上一步
           </button>
-          <h5 class="modal-title" id="exampleModalLabel">建立新貼文</h5>
+          <h5 class="modal-title" id="addPostModalLabel">建立新貼文</h5>
           <!-- 用imgSrc是否有值判斷按鈕是否顯示 -->
           <button
             class="btn position-absolute end-0 mx-3 text-primary"
             v-if="imgSrc"
-            @click="showSideModal"
+            @click="toNextStep"
           >
             下一步
           </button>
@@ -218,8 +267,9 @@ const pushPostToFirebase = function (imageUrl) {
           <div class="modal-body d-flex flex-column align-items-center input-img-area p-0" v-else>
             <img :src="imgSrc" alt="" class="input-img" />
           </div>
+
           <!-- side Modal -->
-          <div class="border-start p-2 w-100" v-if="isModalSideShow">
+          <div class="border-start p-2 w-100" v-if="isSideModalShow">
             <div class="d-flex align-self-center align-middle">
               <div class="rounded-circle user-pic">
                 <img :src="userData.userData.media_url" alt="" />
@@ -229,7 +279,7 @@ const pushPostToFirebase = function (imageUrl) {
             <textarea
               type="text"
               placeholder="撰寫說明文字......"
-              class="postDescriptionInput py-2 w-100"
+              class="post-description-input py-2 w-100"
               v-model="postCaption"
             ></textarea>
           </div>
@@ -250,7 +300,6 @@ input[type='file'] {
 .input-img {
   width: 100%;
 }
-// .modal-dialog,
 .modal-body {
   max-width: 700px;
   max-height: calc(100vh - 6rem - 70px);
@@ -279,7 +328,7 @@ input[type='file'] {
     height: 150%;
   }
 }
-.postDescriptionInput {
+.post-description-input {
   height: calc(100% - 40px);
   border: 0;
   resize: none;
